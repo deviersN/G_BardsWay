696477274cb8bbca1a2a6e1859c79bd63341c9da
deviersN <nicolas.deviers@epitech.eu>
2020-10-26 15:32:00 -0300

    clean home tab

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ import sciter
 
 if __name__ == '__main__':
     frame = sciter.Window(ismain=True, uni_theme=True)
-    frame.load_file("../front/main.html")
+    frame.load_file("../front/home.html")
     frame.expand()
     frame.run_app()
